79d7af3d9109ba3cdeb980747c59de34fb17ab90
locfab <locqueville.fabrice@edu.ece.fr>
2018-11-18 23:02:49 +0100

    projet sans md

--- handles.js
+++ handles.js
@@ -1,7 +1,7 @@
 const url = require('url')
 const qs = require('querystring')
 
-const homeInfo = "Nous somme dans l'url principale, il y a l'url /hello?name=fabrice et il y aura une description de fabrice, parce que je m'appelle fabrice et si vous avez mis votre nom dans le parametre name dans sur l'url, eh bah il y aura hello suivi de ton nom, et juste /hello fera une erreur 404... Tout te semble convenir? C'est parti !! On se retrouve sur l\'url /hello ! \n"
+/*const homeInfo = "Nous somme dans l'url principale, il y a l'url /hello?name=fabrice et il y aura une description de fabrice, parce que je m'appelle fabrice et si vous avez mis votre nom dans le parametre name dans sur l'url, eh bah il y aura hello suivi de ton nom, et juste /hello fera une erreur 404... Tout te semble convenir? C'est parti !! On se retrouve sur l\'url /hello ! \n"
 
 const description = "Fabrice, 22 ans et il y a 6ans et 4mois j'avais 16ans"
 
@@ -38,4 +38,4 @@ module.exports = {
 
     res.end();
   } 
-}
+}*/
--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-const http = require('http')
-const handles = require('./handles')
-const server = http.createServer(handles.serverHandle);
-server.listen(8080);
+express = require('express')
+path = require('path')
+app = express()
+
+app.set('views', "./views")
+app.set('view engine', 'ejs');
+
+app.use('/', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')))
+
+app.set('port', 8080)
+
+app.listen(
+    app.get('port'),
+    () => console.log(`Server listening on ${app.get('port')}`)
+)
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/hello/:name', (req, res) => {
+    res.render('hello', {name: req.params.name})
+})
+
+app.get('*', (req, res) => {
+    res.status(404).render('error', {url: req.url})
+})

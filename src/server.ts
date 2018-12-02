2bdeb0f4c9b3c87e65182b66264ddadb5a1a9033
locfab <locqueville.fabrice@edu.ece.fr>
2018-12-02 19:00:08 +0100

    avancement..

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,116 @@ import bodyparser = require("body-parser");
 const app = express();
 const port: string = process.env.PORT || "8080";
 const dbMet = new MetricsHandler("./db");
+
+
+//USER SESSIONS
+import session = require('express-session')
+import levelSession = require('level-session-store')
+
+const LevelStore = levelSession(session)
+
+
+app.use(session({
+    secret: 'my very secret phrase',
+    store: new LevelStore('./db/sessions'),
+    resave: true,
+    saveUninitialized: true
+}))
+
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded());
-app.get("/", (req: any, res: any) => {
-    res.write("Hello world");
-    res.send();
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+
+//USER AUTHENTICATION 1
+import { UserHandler, User } from './user'
+const dbUser: UserHandler = new UserHandler('./db/users')
+
+app.get('/login', (req: any, res: any) => {
+    res.render('login')
+})
+
+app.get('/logout', (req: any, res: any) => {
+    delete req.session.loggedIn
+    delete req.session.user
+    res.redirect('/login')
+})
+
+//USER AUTHENTICATION 2
+app.post('/login', (req: any, res: any, next: any)  => {
+    dbUser.get(req.body.username, (err: Error | null, result?: User) => {
+        if (err) next(err)
+        if (result === undefined || !result.validatePassword(req.body.password)) {
+            res.redirect('/login')
+        } else {
+            req.session.loggedIn = true
+            req.session.user = result
+            res.redirect('/')
+        }
+    })
+})
+
+//USER CREATION AND RETRIEVAL
+const userRouter = express.Router()
+
+userRouter.post('/', (req: any, res: any, next: any)  => {
+    const { username, password, email } = req.body
+    const u = new User(username, password, email)
+    dbUser.save(u, (err: Error | null) => {
+        if (err) next(err)
+        res.status(200).send("user saved")
+    })
+})
+
+userRouter.get('/', (req: any, res: any, next: any)  => {
+    dbUser.get(req.body.username, (err: Error | null, result?: User) => {
+        if (err) next(err)
+        if (result === undefined || !result.validatePassword(req.body.username)) {
+            res.status(404).send("user not found")
+        } else {
+            res.status(200).json(result)
+        }
+    })
+})
+
+app.use(userRouter);
+
+const authCheck = function (req: any, res: any, next: any) {
+    if (req.session.loggedIn) {
+        next()
+    } else res.redirect('/login')
+}
+
+app.get('/', authCheck, (req: any, res: any) => {
+    res.render('index', { name: req.session.username })
+})
+
+
+//USER AUTHORIZATION MIDDLEWARE
+
+app.get('/signup', (req, res) => {
+    res.render('signup')
+})
+
+
+
+app.listen(port, (err: Error) => {
+    if (err) throw err;
+    console.log(`Server is listening on port ${port}`);
 });
+
+
+
+
+
+
+
+
+
+
+
 app.get("/metrics/:id", (req: any, res: any) => {
     dbMet.get(req.params.id, (err: Error | null, result?: Metric[]) => {
         if (err) throw err;
@@ -37,9 +141,4 @@ app.delete("/metrics/:id", (req, res) => {
         }
         res.status(200).send();
     });
-});
-
-app.listen(port, (err: Error) => {
-    if (err) throw err;
-    console.log(`Server is listening on port ${port}`);
 });
---
+++ src/user.ts
@@ -0,0 +1,74 @@
+import { LevelDb } from "./leveldb"
+import WriteStream from 'level-ws'
+
+export class User {
+    public username: string
+    public email: string
+    private password: string = ""
+
+    constructor(username: string, email: string, password: string, passwordHashed: boolean = false) {
+        this.username = username
+        this.email = email
+
+        if (!passwordHashed) {
+            this.setPassword(password)
+        } else this.password = password
+    }
+    static fromDb(data: any): User {
+        return new User("a","b","c")
+    }
+
+    public setPassword(toSet: string): void {
+        this.password = toSet
+    }
+
+    public getPassword(): string {
+        return this.password
+    }
+
+    public validatePassword(toValidate: String): boolean {
+        return this.password === toValidate
+    }
+}
+
+
+export class UserHandler {
+    public db: any
+
+    public get(username: string, callback: (err: Error | null, result?: User) => void) {
+        this.db.get(`user:${username}`, function (err: Error, data: any) {
+            if (err) callback(err)
+            else if (data === undefined) callback(null, data)
+            else callback(null, User.fromDb(data))
+        })
+    }
+
+    public save(user: any, callback: (err: Error | null) => void) {
+
+        user = new User(user.username, user.email, user.password)
+
+        this.db.put(
+            `user:${user.username}`,
+            `${user.getPassword()}:${user.email}`,
+            (err: Error | null) => {
+                callback(err)
+            }
+        )
+    }
+
+    public remove(username: string, callback: (err: Error | null) => void) {
+
+        this.db.del(
+            `user:${username}`,
+            (error: Error | null) => {
+
+                if (error) callback(error)
+                else callback(null)
+            }
+        )
+    }
+
+    constructor(path: string) {
+        this.db = LevelDb.open(path)
+    }
+}
